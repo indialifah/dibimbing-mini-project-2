b272fce68b1e4ba1e6e1da1266299d1b758b095b
indialifah <indialifah25@gmail.com>
2024-08-01 20:20:13 +0700

    add logic to handle change on register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleChangeUsername = (a) => {
+    setUsername(a.target.value)
+  }
+  const handleChangePassword = (a) => {
+    setPassword(a.target.value)
+  }
+  const handleRegister = () => {
+    try {
+      // disini buat simpan data buat akun
+      console.log('username: '+ username + ' password: '+ password)
+    }
+    catch (err) {
+      console.log(err.response)
+    }
+  }
+
   return (
     <div className='wrapper'>
         <div className='container'>
@@ -9,12 +29,12 @@ const Register = () => {
             <div className='form-container'>
                 
                 <p>Username</p>
-                <input type="text" placeholder='username'/>
+                <input onChange={handleChangeUsername} type="text" placeholder='username'/>
                 
                 <p>Password</p>
-                <input type="text" placeholder='password'/>
+                <input onChange={handleChangePassword} type="text" placeholder='password'/>
 
-                <button>Register</button>
+                <button onClick={handleRegister}>Register</button>
             </div>
             <Link to='/login'>already have an account</Link>
         </div>
