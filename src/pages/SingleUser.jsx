52c7269312b103159a74c07f66dfe9f6614308a4
indialifah <indialifah25@gmail.com>
2024-08-01 20:42:58 +0700

    make single-user page structure

--- src/pages/SingleUser.jsx
+++ src/pages/SingleUser.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const SingleUser = () => {
   return (
-    <div>SingleUser</div>
+    <div className='wrapper'>
+      <div className='container'>
+        <h1>Single User</h1>
+        <hr />
+        <img src="" alt="avatar" />
+        <h3>First Name</h3>
+        <h3>Last Name</h3>
+        <h3>Email</h3>
+      </div>
+    </div>
   )
 }
 
