abe3693aaa0323c9bd041d7908bb56f9d9285176
indialifah <indialifah25@gmail.com>
2024-08-07 11:35:42 +0700

    style login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,21 +41,44 @@ const Login = () => {
   }
 
   return (
-    <div className='wrapper'>
-        <div className='container'>
-            <h1>Login</h1>
-            <div className='form-container'>
+    <div className="grid place-items-center h-screen">
+    <div className='md:w-3/5 mx-auto bg-white bg-opacity-85 shadow-lg rounded-xl'>
+      <div className='flex'>
+          <div className='md:w-2/5 bg-sky-500 rounded-lg m-2'></div>
+          <div className='md:w-3/5 w-full p-8'>
+            <h1 className='lg:text-3xl md:text-2xl text-3xl font-bold'>Welcome back! 👋</h1>
+            <p className='py-1'>Please log in to your account</p>
+
+              <div className='mt-10'>
+                <div className='md:my-2 my-8'>
+                  <p>E-mail</p>
+                  <input onChange={handleChangeEmail} type="text" placeholder='E-mail'
+                    className='my-2 rounded-xl p-2 w-[100%] focus:outline-none focus:border-sky-600 border-2'/>
+                </div>
+                <div className='md:my2 my-8'>
+                  <p>Password</p>
+                  <input onChange={handleChangePassword} type="text" placeholder='Password'
+                    className="my-2 rounded-xl p-2 w-[100%] focus:outline-none focus:border-sky-600 border-2"/>
+                </div>
                 
-                <p>Email</p>
-                <input onChange={handleChangeEmail} type="text" placeholder='email'/>
-                
-                <p>Password</p>
-                <input onChange={handleChangePassword} type="text" placeholder='password'/>
-                
-                <button onClick={handleLogin}>Login</button>
-            </div>
-            <Link to='/register'>create an account</Link>
-        </div>
+                <div className='flex flex-col'>
+                  <button 
+                    onClick={handleLogin} 
+                    className="w-[100%] my-4 py-3 bg-sky-500 text-white text-lg rounded-xl hover:bg-sky-700"
+                  >
+                    Login
+                  </button>
+                  <div className="flex gap-1 justify-center md:text-sm text-xs">
+                    <p>Don't have an account?</p>
+                    <Link to='/register' className="underline text-sky-500 hover:text-sky-700">Create new</Link>
+                  </div>
+                </div>
+
+              </div>
+
+          </div>
+      </div>
+    </div>
     </div>
   )
 }
