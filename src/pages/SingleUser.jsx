cf06311343379b9552cf0cbdc4b5332cd947e692
indialifah <indialifah25@gmail.com>
2024-08-07 13:49:45 +0700

    style single-user page

--- src/pages/SingleUser.jsx
+++ src/pages/SingleUser.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 const SingleUser = () => {
 
@@ -23,22 +23,38 @@ const SingleUser = () => {
   }, [id])
 
   return (
-    <div className='wrapper'>
-      <div className='container'>
-        <h1>Single User</h1>
-        <hr />
+    <div>
+      <h1 className='text-3xl font-semibold p-10 bg-sky-700 text-white'>Single User</h1>
+      {/* <hr /> */}
+      <div className='my-8 flex justify-center content-center'>
         {
           user && (
-            <>
-              <img src={user.avatar} alt="avatar" />
-              <h3>First Name: {user.first_name}</h3>
-              <h3>Last Name: {user.last_name}</h3>
-              <h3>Email: {user.email}</h3>
-            </>
+            <div className='p-8 bg-white bg-opacity-50 shadow-lg rounded-xl'>
+              <div className=' flex flex-col gap-y-4 items-center'>
+                <img src={user.avatar} alt="avatar" className='w-40 rounded-full'/>
+                <h3 className='text-2xl font-bold'>{user.first_name} {user.last_name}</h3>
+                <hr className='w-[100%] border-sky-700 border-1'/>
+                <div className='grid grid-cols-2'>
+                  <div>
+                    <p className='text-xl font-medium'>First Name: </p>
+                    <p className='text-xl font-medium'>Last Name: </p>
+                    <p className='text-xl font-medium'>Email: </p>
+                  </div>
+                  <div className='text-right'>
+                    <p className='text-xl font-semilight'>{user.first_name}</p>
+                    <p className='text-xl font-semilight'>{user.last_name}</p>
+                    <p className='text-xl font-semilight'>{user.email}</p>
+                  </div>
+                </div>
+              </div>
+              <Link to={'/'} className='float-right mt-8 py-2 px-6 bg-sky-500 text-white text-lg rounded-full hover:bg-sky-700'>
+                Back
+              </Link>
+            </div>
           )
         }
-        
       </div>
+      
     </div>
   )
 }
