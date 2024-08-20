a5443d271e3d67ba7a7a83fc9b40efc8deff1370
indialifah <indialifah25@gmail.com>
2024-08-20 09:17:08 +0700

    fix handle logout

--- src/pages/ListUser.jsx
+++ src/pages/ListUser.jsx
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const ListUser = () => {
     
     const [users, setUsers] = useState([])
+
+    const navigate = useNavigate()
+
     const getUsers = () => {
         axios
             .get('https://reqres.in/api/users')
